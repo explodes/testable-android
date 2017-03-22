edbf58fa1af084707097fef0477291461131629d
Evan Leis <evan.explodes@gmail.com>
2017-03-22 00:52:35 -0600

    [query-db] remove query-db library, use query-db as jitpack dep

--- query-db/src/main/java/io/explod/querydb/db/Migration.java
+++
@@ -1,8 +0,0 @@
-package io.explod.querydb.db;
-
-import android.database.sqlite.SQLiteDatabase;
-import android.support.annotation.NonNull;
-
-public interface Migration {
-	void execute(@NonNull SQLiteDatabase db);
-}
